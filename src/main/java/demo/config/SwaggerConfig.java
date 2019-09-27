a6c086804c7239a6d057b88a7961dbf9d758492a
q39676 <396767101@qq.com>
2019-09-27 13:19:50 +0800

    .

--- src/main/java/demo/config/SwaggerConfig.java
+++ src/main/java/demo/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import demo.base.system.service.impl.SystemConstantService;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,15 +18,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
+	@Autowired
+	private SystemConstantService constantService;
+	
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-        		.apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+		Docket d = null;
+		String envName = constantService.getValByName("envName");
+		if ("dev".contentEquals(envName)) {
+			d = new Docket(DocumentationType.SWAGGER_2)
+					.apiInfo(apiInfo())
+					.select()
+					.apis(RequestHandlerSelectors.any())
+					.paths(PathSelectors.any()).build();
+		} else {
+			d = new Docket(DocumentationType.SWAGGER_2)
+					.apiInfo(apiInfo())
+					.select()
+					.apis(RequestHandlerSelectors.basePackage(""))
+					.paths(PathSelectors.any())
+					.build();
+		}
+		return d;
+	}
     
 	private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
