10f6ea23e31b4c392b601374abc6bfae7562a882
q39676 <396767101@qq.com>
2019-11-12 10:40:59 +0800

    .

--- src/main/java/demo/movie/service/MovieManagerService.java
+++ src/main/java/demo/movie/service/MovieManagerService.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 
 public interface MovieManagerService {
 
+	/**
+	 * 定期任务调用
+	 * 删除超长期限的资源(物理删除)
+	 * @throws IOException
+	 */
 	void deleteOldHistory() throws IOException;
 
 }
