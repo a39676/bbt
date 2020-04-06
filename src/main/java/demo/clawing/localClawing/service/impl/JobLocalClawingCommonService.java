1a2221abc3ef21697a9b18a7aa296f2e1ee36ab0
DavenChan <396767101@qq.com>
2020-04-06 18:19:27 +0800

    JobLocalClawingCommonService update

--- src/main/java/demo/clawing/localClawing/service/impl/JobLocalClawingCommonService.java
+++ src/main/java/demo/clawing/localClawing/service/impl/JobLocalClawingCommonService.java
@@ -7,7 +7,10 @@ import demo.selenium.service.impl.SeleniumCommonService;
 public abstract class JobLocalClawingCommonService extends SeleniumCommonService {
 
 	/* 不察看的公司名(contain?) */
-	protected static List<String> filterName = List.of("联想利泰", "腾讯", "网易", "阿里", "合众", "蜜源", "华为");
+	protected static List<String> filterName = List.of(
+			"联想利泰", "腾讯", "网易", "阿里", "合众", "蜜源", "华为", "东方思维", "三盟",
+			"新大陆金融", "网商微贷", "贷", "游戏"
+			);
 	
 	/* 每个关键词, 进入点击条数 */
 	protected static final int keywordClickMaxCount = 50;
