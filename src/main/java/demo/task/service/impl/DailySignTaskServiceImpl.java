5fb39902c1cc10d64ee6a86f765f750986031bce
DavenChan <396767101@qq.com>
2020-04-08 21:29:54 +0800

    jobLocalClawing 黑名更新; 51job 夜间降低频率;

--- src/main/java/demo/task/service/impl/DailySignTaskServiceImpl.java
+++ src/main/java/demo/task/service/impl/DailySignTaskServiceImpl.java
@@ -1,5 +1,8 @@
 package demo.task.service.impl;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,7 +18,13 @@ public class DailySignTaskServiceImpl extends SeleniumTaskCommonServiceImpl {
 	@Scheduled(fixedRate = 1000L * 60 * 5)
 	public void insertWuYiSign() {
 		if(!"dev".equals(constantService.getValByName("envName"))) {
-			wuyiService.insertDailySignEvent();
+			LocalDateTime now = LocalDateTime.now();
+			if(now.getHour() >= 22 || now.getHour() <= 8) {
+				if(ThreadLocalRandom.current().nextInt(1, 4) <= 1) {
+					wuyiService.insertDailySignEvent();
+				}
+			}
+			;
 		}
 	}
 }
