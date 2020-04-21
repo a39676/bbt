a462495b32759e60f71fa106736f7fcb0b6d2ae9
DavenChan <396767101@qq.com>
2020-04-21 16:48:17 +0800

    删除冗余

--- src/main/java/demo/geographical/service/GeographicalService.java
+++
@@ -1,9 +0,0 @@
-package demo.geographical.service;
-
-import demo.geographical.pojo.po.GeographicalArea;
-
-public interface GeographicalService {
-
-	GeographicalArea findGeographical(Long geographicalId);
-
-}
