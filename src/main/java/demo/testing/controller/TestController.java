8eca5b8a807387d7fb489028d9b52d220e2efb64
DavenChan <396767101@qq.com>
2020-04-21 16:54:09 +0800

    删除冗余

--- src/main/java/demo/testing/controller/TestController.java
+++ src/main/java/demo/testing/controller/TestController.java
@@ -2,14 +2,9 @@ package demo.testing.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import demo.baseCommon.controller.CommonController;
-import demo.clawing.medicine.service.ClawingSinaMedicineService;
-import demo.clawing.neobux.service.NeobuxService;
-import demo.selenium.service.SeForJoke;
-import demo.selenium.service.SeleniumService;
 import demo.testing.pojo.constant.TestUrl;
 import demo.testing.service.TestService;
 
@@ -20,51 +15,5 @@ public class TestController extends CommonController {
 	@SuppressWarnings("unused")
 	@Autowired
 	private TestService testService;
-	@Autowired
-	private SeleniumService seleniumService;
-	
-	@Autowired
-	private SeForJoke seForJoke;
-	@Autowired
-	private ClawingSinaMedicineService medicineService;
-	@Autowired
-	private NeobuxService nbService;
-	
-	
-	@GetMapping(value = "/seleniumTest")
-	public void seleniumTest() { 
-		seleniumService.testDemo();
-	}
-	
-	@GetMapping(value = "/logTest")
-	public void logTest() {
-		String str = "strrrrrr";
-		log.info("something {}", str);
-	}
-	
-	@GetMapping(value = "sjlanxiang")
-	public void sjlanxiang() {
-		seForJoke.lanXiang();
-	}
-	
-	@GetMapping(value = "sjliuxue")
-	public void sjLiuXue() {
-		seForJoke.liuXue();
-	}
-
-	@GetMapping(value = "sjtest")
-	public void sjTest() {
-		seForJoke.test();
-	}
-	
-	@GetMapping(value = "mtest")
-	public void mTest() {
-		medicineService.medicineTest();
-	}
-	
-	@GetMapping(value = "nbtest")
-	public void noebuxTest() {
-		nbService.test1();
-	}
 	
 }
--- src/main/java/demo/testing/pojo/constant/TestUrl.java
+++ src/main/java/demo/testing/pojo/constant/TestUrl.java
@@ -3,6 +3,5 @@ package demo.testing.pojo.constant;
 public class TestUrl {
 	
 	public static final String testRoot = "/test";
-	public static final String testRoot2 = "/test2";
 
 }
