d8025ee74cc695dd9fb545657e1c5d349f19e011
DavenChan <396767101@qq.com>
2020-07-05 18:34:14 +0800

    fix

--- src/main/java/demo/interaction/preicous_metal/service/PreciousMetalTransService.java
+++ src/main/java/demo/interaction/preicous_metal/service/PreciousMetalTransService.java
@@ -1,7 +1,6 @@
 package demo.interaction.preicous_metal.service;
 
 import auxiliaryCommon.pojo.result.CommonResult;
-import image.pojo.dto.UploadImageToCloudinaryDTO;
 import precious_metal.pojo.dto.TransPreciousMetalPriceDTO;
 
 public interface PreciousMetalTransService {
