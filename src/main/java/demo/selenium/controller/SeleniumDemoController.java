d57f39bc4949ec4c97a8b52ed4d5b4b95d0faf01
DavenChan <396767101@qq.com>
2020-08-28 15:50:34 +0800

    删除冗余

--- src/main/java/demo/selenium/controller/SeleniumDemoController.java
+++ src/main/java/demo/selenium/controller/SeleniumDemoController.java
@@ -1,24 +1,10 @@
 package demo.selenium.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import demo.selenium.service.SeForJoke;
 
 @Controller
-@RequestMapping("/se")
+@RequestMapping("/selenium")
 public class SeleniumDemoController {
 
-	@Autowired
-	private SeForJoke joke;
-	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		joke.test();
-		return "hi";
-	}
 }
