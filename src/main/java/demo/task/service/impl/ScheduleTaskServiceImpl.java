5f4edcd4357830abff4ae88dfb40cd0c7052eed4
DavenChan <396767101@qq.com>
2020-10-26 11:53:36 +0800

    .

--- src/main/java/demo/task/service/impl/ScheduleTaskServiceImpl.java
+++ src/main/java/demo/task/service/impl/ScheduleTaskServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import demo.clawing.scheduleClawing.service.CryptoCoinPriceService;
-import demo.clawing.scheduleClawing.service.PreciousMetalsPriceService;
 import demo.clawing.scheduleClawing.service.WuYiJobRefreshService;
 
 @Component
@@ -16,8 +15,8 @@ public class ScheduleTaskServiceImpl extends SeleniumTaskCommonServiceImpl {
 
 	@Autowired
 	private WuYiJobRefreshService wuyiService;
-	@Autowired
-	private PreciousMetalsPriceService preciousMetalsPriceService;
+//	@Autowired
+//	private PreciousMetalsPriceService preciousMetalsPriceService;
 	@Autowired
 	private CryptoCoinPriceService cryptoCoinPriceService;
 	
@@ -37,74 +36,72 @@ public class ScheduleTaskServiceImpl extends SeleniumTaskCommonServiceImpl {
 		}
 	}
 	
-	@Scheduled(cron = "*/30 * * * * ?")
-	public void insertCryptoCoinNewPriceCollect() {
-//		TODO
+//	@Scheduled(cron = "*/60 * * * * ?")
+//	public void insertCryptoCoinNewPriceCollect() {
 //		if (!"dev".equals(constantService.getValByName("envName"))) {
+//			cryptoCoinPriceService.insertNewCryptoCoinPriceEvent();
 //		}
-		cryptoCoinPriceService.insertNewCryptoCoinPriceEvent();
-	}
+//	}
 	
-	@Scheduled(cron = "0 */8 * * * ?")
+	@Scheduled(cron = "0 */1 * * * ?")
 	public void insertCryptoCoinHistoryPriceCollect() {
-//		TODO
-//		if (!"dev".equals(constantService.getValByName("envName"))) {
+		if (!"dev".equals(constantService.getValByName("envName"))) {
+			cryptoCoinPriceService.insertHistoryCryptoCoinPriceEvent();
+		}
+	}
+	
+//	@Scheduled(cron = "*/30 * * * * ?")
+//	public void insertMetalsPriceClaw() {
+//		/*
+//		 * TODO
+//		 * 2020-07-16
+//		 * 大概率损失 每周 开盘 & 收盘价, 
+//		 * 交给cx 通过api 加入任务?
+//		 * 需要设法捕捉
+//		 */
+//		if (!"dev".equals(constantService.getValByName("envName"))
+//				&& isPreciousMetalsTransactionTime()) {
+//			preciousMetalsPriceService.insertClawingEvent();
 //		}
-		cryptoCoinPriceService.insertHistoryCryptoCoinPriceEvent();
-	}
-	
-	@Scheduled(cron = "*/30 * * * * ?")
-	public void insertMetalsPriceClaw() {
-		/*
-		 * TODO
-		 * 2020-07-16
-		 * 大概率损失 每周 开盘 & 收盘价, 
-		 * 交给cx 通过api 加入任务?
-		 * 需要设法捕捉
-		 */
-		if (!"dev".equals(constantService.getValByName("envName"))
-				&& isPreciousMetalsTransactionTime()) {
-			preciousMetalsPriceService.insertClawingEvent();
-		}
-	}
-	
-	/*
-	 * TODO
-	 * 2020-07-16
-	 * 需要解耦
-	 * 以下"时间" 统一为北京时间
-	 * 
-	 * 1. 输入时间, 判断是否交易时间
-	 * 2. 交易时间, 
-	 */
-	private boolean isPreciousMetalsTransactionTime() {
-		LocalDateTime beiJingNow = LocalDateTime.now();
-		LocalDateTime washtonNow = beiJingNow.minusHours(12);
-		int dayOfWeek = beiJingNow.getDayOfWeek().getValue();
-		
-		/*
-		 * 2020-07-06
-		 * 理论上美国黄金交易时间为: 
-		 * (北京时间) 周一06:60 ~ 周六 03:30
-		 * 冬令时期间 延后半小时
-		 */
-		boolean isUSWinterTime = localDateTimeHandler.isUSWinterTime(washtonNow.toLocalDate());
-		if(isUSWinterTime) {
-			beiJingNow = beiJingNow.minusMinutes(30);
-		}
-		
-		if (dayOfWeek == 1) {
-			return (beiJingNow.getHour() >= 6 && beiJingNow.getMinute() >= 30);
-		} else if (dayOfWeek > 1 && dayOfWeek < 6) {
-			return true;
-		} else if (dayOfWeek == 6) {
-			return (beiJingNow.getHour() <= 3 && beiJingNow.getMinute() <= 30);
-		} else if (dayOfWeek > 6) {
-			return false;
-		}
-
-		return false;
-	}
+//	}
+//	
+//	/*
+//	 * TODO
+//	 * 2020-07-16
+//	 * 需要解耦
+//	 * 以下"时间" 统一为北京时间
+//	 * 
+//	 * 1. 输入时间, 判断是否交易时间
+//	 * 2. 交易时间, 
+//	 */
+//	private boolean isPreciousMetalsTransactionTime() {
+//		LocalDateTime beiJingNow = LocalDateTime.now();
+//		LocalDateTime washtonNow = beiJingNow.minusHours(12);
+//		int dayOfWeek = beiJingNow.getDayOfWeek().getValue();
+//		
+//		/*
+//		 * 2020-07-06
+//		 * 理论上美国黄金交易时间为: 
+//		 * (北京时间) 周一06:60 ~ 周六 03:30
+//		 * 冬令时期间 延后半小时
+//		 */
+//		boolean isUSWinterTime = localDateTimeHandler.isUSWinterTime(washtonNow.toLocalDate());
+//		if(isUSWinterTime) {
+//			beiJingNow = beiJingNow.minusMinutes(30);
+//		}
+//		
+//		if (dayOfWeek == 1) {
+//			return (beiJingNow.getHour() >= 6 && beiJingNow.getMinute() >= 30);
+//		} else if (dayOfWeek > 1 && dayOfWeek < 6) {
+//			return true;
+//		} else if (dayOfWeek == 6) {
+//			return (beiJingNow.getHour() <= 3 && beiJingNow.getMinute() <= 30);
+//		} else if (dayOfWeek > 6) {
+//			return false;
+//		}
+//
+//		return false;
+//	}
 
 //	@Autowired
 //	private MaiMaiScheduleClawingServiceImpl maiMaiLocalClawingServiceImpl;
