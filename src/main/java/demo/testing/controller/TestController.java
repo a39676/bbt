669b3da2f8477abb8c1334cc8a8adf5373152f58
DavenChan <396767101@qq.com>
2021-01-28 10:22:15 +0800

    after debug

--- src/main/java/demo/testing/controller/TestController.java
+++ src/main/java/demo/testing/controller/TestController.java
@@ -1,15 +1,10 @@
 package demo.testing.controller;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import demo.baseCommon.controller.CommonController;
-import demo.selenium.service.OldDataDeleteService;
 import demo.testing.pojo.constant.TestUrl;
 import demo.testing.service.TestService;
 
@@ -21,17 +16,4 @@ public class TestController extends CommonController {
 	@Autowired
 	private TestService testService;
 
-	@Autowired
-	private OldDataDeleteService oldDataDeleteService;
-	
-	@GetMapping(value = "/deleteOldReport")
-	@ResponseBody
-	public String deleteOldReport() {
-		try {
-			oldDataDeleteService.deleteOldReport();
-			return "done";
-		} catch (IOException e) {
-			return e.getMessage();
-		}
-	}
 }
