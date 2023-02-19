b92083e87596cbfa8b5852764d4b9f29d681fec3
DavenChan <demo@demo.com>
2023-02-19 12:18:44 +0800

    Debug

---
+++ src/main/java/demo/toyParts/runShellScript/RunShellScript.java
@@ -0,0 +1,25 @@
+package demo.toyParts.runShellScript;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/testSh")
+public class RunShellScript {
+
+	@GetMapping("/run")
+	@ResponseBody
+	public String run() {
+		try {
+			ProcessBuilder builder = new ProcessBuilder();
+			builder.command(" sh /home/u2/tmp/tmp.sh");
+			builder.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
+		return "done";
+	}
+}
