cd17f2eed90a890d93449888698a43a4856d54a4
DavenChan <demo@demo.com>
2023-03-23 20:59:59 +0800

    .

--- src/main/java/demo/experiment/controller/TestController.java
+++ src/main/java/demo/experiment/controller/TestController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import autoTest.testEvent.common.pojo.dto.AutomationTestInsertEventDTO;
-import autoTest.testEvent.scheduleClawing.pojo.type.ScheduleClawingType;
-import autoTest.testModule.pojo.type.TestModuleType;
-import demo.autoTestBase.testEvent.pojo.bo.TestEventBO;
-import demo.autoTestBase.testEvent.service.TestEventService;
 import demo.baseCommon.controller.CommonController;
 import demo.experiment.pojo.constant.TestUrl;
 import demo.experiment.service.TestService;
@@ -28,19 +23,4 @@ public class TestController extends CommonController {
 		return testService.testing("something");
 	}
 
-	@Autowired
-	private TestEventService testEventService;
-
-	@GetMapping(value = "/t1")
-	@ResponseBody
-	public TestEventBO getDailyData() throws Exception {
-
-		AutomationTestInsertEventDTO eventDTO = new AutomationTestInsertEventDTO();
-		eventDTO.setTestEventId(0L);
-		eventDTO.setTestModuleType(TestModuleType.SCHEDULE_CLAWING.getId());
-		eventDTO.setFlowType(ScheduleClawingType.CRYPTO_COIN.getId());
-
-		return testEventService.reciveTestEventAndRun(eventDTO);
-	}
-	
 }
