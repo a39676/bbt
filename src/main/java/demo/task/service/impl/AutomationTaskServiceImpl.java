11e550af11301ece452c65e17cc6d905c6a400c9
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-12-24 14:55:55 +0800

    Update

--- src/main/java/demo/task/service/impl/AutomationTaskServiceImpl.java
+++ src/main/java/demo/task/service/impl/AutomationTaskServiceImpl.java
@@ -2,6 +2,7 @@ package demo.task.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import autoTest.testEvent.common.pojo.dto.AutomationTestInsertEventDTO;
 import autoTest.testEvent.scheduleClawing.pojo.type.ScheduleClawingType;
@@ -9,21 +10,22 @@ import autoTest.testModule.pojo.type.TestModuleType;
 import demo.baseCommon.service.CommonService;
 import demo.task.service.impl.mq.producer.TestEventInsertAckProducer;
 
+@Service
 public class AutomationTaskServiceImpl extends CommonService {
 
 	@Autowired
 	private TestEventInsertAckProducer testEventInsertAckProducer;
-	
-	@Scheduled(cron="20 19 09 * * *")
-	@Scheduled(cron="20 19 12 * * *")
-	@Scheduled(cron="20 19 17 * * *")
-	@Scheduled(cron="20 19 20 * * *")
-	@Scheduled(cron="20 19 23 * * *")
+
+	@Scheduled(cron = "20 19 09 * * *")
+	@Scheduled(cron = "20 19 12 * * *")
+	@Scheduled(cron = "20 19 17 * * *")
+	@Scheduled(cron = "20 19 20 * * *")
+	@Scheduled(cron = "20 19 23 * * *")
 	public void sendNormalDataTask() {
 		sendEducationInfomationCollectionTask();
 		sendV2exJobInfomationCollectionTask();
 	}
-	
+
 	private void sendEducationInfomationCollectionTask() {
 		AutomationTestInsertEventDTO dto = new AutomationTestInsertEventDTO();
 		dto.setTestModuleType(TestModuleType.SCHEDULE_CLAWING.getId());
@@ -31,7 +33,7 @@ public class AutomationTaskServiceImpl extends CommonService {
 		dto.setTestEventId(snowFlake.getNextId());
 		testEventInsertAckProducer.send(dto);
 	}
-	
+
 	private void sendV2exJobInfomationCollectionTask() {
 		AutomationTestInsertEventDTO dto = new AutomationTestInsertEventDTO();
 		dto.setTestModuleType(TestModuleType.SCHEDULE_CLAWING.getId());
