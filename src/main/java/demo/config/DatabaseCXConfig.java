f16d58a551f25303cc0ac3f8eb6f64a09cdf02c5
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-12-26 23:06:39 +0800

    Update

--- src/main/java/demo/config/DatabaseCXConfig.java
+++ src/main/java/demo/config/DatabaseCXConfig.java
@@ -9,6 +9,7 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -20,11 +21,25 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 @MapperScan(basePackages = { "demo.toyParts.multipleDB.mapper" }, sqlSessionTemplateRef = "cxSqlSessionTemplate")
 public class DatabaseCXConfig {
+	
+	@Value("${databaseCX.driverClassName}")
+  	private String driverClassName;
+  	@Value("${databaseCX.url}")
+  	private String url;
+  	@Value("${databaseCX.username}")
+  	private String username;
+  	@Value("${databaseCX.password}")
+  	private String password;
 
 	@Bean(name = "cxDataSourceProperties")
 	@ConfigurationProperties(prefix = "spring.datasource.cx")
 	public DataSourceProperties cxDataSourceProperties() {
-		return new DataSourceProperties();
+		DataSourceProperties d = new DataSourceProperties();
+		d.setDriverClassName(driverClassName);
+		d.setUrl(url);
+		d.setUsername(username);
+		d.setPassword(password);
+		return d;
 	}
 
 	@Bean(name = "cxDataSource")
