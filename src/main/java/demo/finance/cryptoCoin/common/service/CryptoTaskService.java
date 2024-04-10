d6f55911f93fc191f9f939d5785a9424d6210087
Daven <19781485+a39676@users.noreply.github.com>
2024-04-10 23:23:33 +0800

    Update

--- src/main/java/demo/finance/cryptoCoin/common/service/CryptoTaskService.java
+++ src/main/java/demo/finance/cryptoCoin/common/service/CryptoTaskService.java
@@ -1,44 +1,46 @@
-package demo.finance.cryptoCoin.common.service;
+// 2024-04-10 Remove to crypto coin monitor project
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import demo.finance.cryptoCoin.data.service.CryptoCoinComplexService;
-
-@Component
-public class CryptoTaskService {
-
-	@Autowired
-	private CryptoCoinComplexService cryptoCoinComplexService;
-
-	@Scheduled(fixedDelay = 1000L * 30)
-	public void deleteOldData() {
-		cryptoCoinComplexService.deleteOld1MinKLineDatas();
-	}
-
-	@Scheduled(cron = "* */10 * * * *")
-	public void checkBigMoveInMinutes() {
-		cryptoCoinComplexService.checkBigMoveInMinutes();
-	}
-
-	@Scheduled(cron = "* */5 * * * *")
-	public void checkBigMoveInHours() {
-		cryptoCoinComplexService.checkBigMoveInHours();
-	}
-
-	@Scheduled(fixedDelay = 1000L * 30)
-	public void checkBinanceKLineStreamAliveAndReconnect() {
-		cryptoCoinComplexService.checkBinanceKLineStreamAliveAndReconnect();
-	}
-
-	@Scheduled(fixedDelay = 1000L * 60)
-	public void getRecentBigMoveCounter() {
-		cryptoCoinComplexService.getRecentBigMoveCounterBySymbol();
-	}
-
-	@Scheduled(fixedDelay = 1000L * 60 * 10)
-	public void getCryptoCoinOptionFromCthulhu() {
-		cryptoCoinComplexService.getCryptoCoinOptionFromCthulhu();
-	}
-}
+//package demo.finance.cryptoCoin.common.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import demo.finance.cryptoCoin.data.service.CryptoCoinComplexService;
+//
+//@Component
+//public class CryptoTaskService {
+//
+//	@Autowired
+//	private CryptoCoinComplexService cryptoCoinComplexService;
+//
+//	@Scheduled(fixedDelay = 1000L * 30)
+//	public void deleteOldData() {
+//		cryptoCoinComplexService.deleteOld1MinKLineDatas();
+//	}
+//
+//	@Scheduled(cron = "* */10 * * * *")
+//	public void checkBigMoveInMinutes() {
+//		cryptoCoinComplexService.checkBigMoveInMinutes();
+//	}
+//
+//	@Scheduled(cron = "* */5 * * * *")
+//	public void checkBigMoveInHours() {
+//		cryptoCoinComplexService.checkBigMoveInHours();
+//	}
+//
+//	@Scheduled(fixedDelay = 1000L * 30)
+//	public void checkBinanceKLineStreamAliveAndReconnect() {
+//		cryptoCoinComplexService.checkBinanceKLineStreamAliveAndReconnect();
+//	}
+//
+//	@Scheduled(fixedDelay = 1000L * 60)
+//	public void getRecentBigMoveCounter() {
+//		cryptoCoinComplexService.getRecentBigMoveCounterBySymbol();
+//	}
+//
+//	@Scheduled(fixedDelay = 1000L * 60 * 10)
+//	public void getCryptoCoinOptionFromCthulhu() {
+//		cryptoCoinComplexService.getCryptoCoinOptionFromCthulhu();
+//	}
+//}
