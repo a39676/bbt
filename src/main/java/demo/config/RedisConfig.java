2f59fdd8197b39e825da78e605272240e196e104
Daven <19781485+a39676@users.noreply.github.com>
2024-04-24 19:03:44 +0800

    Update

--- src/main/java/demo/config/RedisConfig.java
+++ src/main/java/demo/config/RedisConfig.java
@@ -1,38 +1,67 @@
 package demo.config;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-	
+
+	@Value("${redis.host}")
+	private String host;
+	@Value("${redis.port}")
+	private Integer port;
+	@Value("${redis.password}")
+	private String password;
+
+	@Bean
+	RedisConnectionFactory redisConnectionFactory() throws IOException {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(host);
+		redisStandaloneConfiguration.setPort(port);
+		redisStandaloneConfiguration.setPassword(password);
+
+		LettuceClientConfiguration.LettuceClientConfigurationBuilder lettuceClientConfigurationBuilder = LettuceClientConfiguration
+				.builder();
+
+		LettuceClientConfiguration lettuceClientConfiguration = lettuceClientConfigurationBuilder.build();
+
+		return new LettuceConnectionFactory(redisStandaloneConfiguration, lettuceClientConfiguration);
+	}
+
 	/**
-     *  定义 StringRedisTemplate , 指定序列化和反序列化的处理类
-     * @param factory
-     * @return
-     */
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-    	RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+	 * 定义 StringRedisTemplate , 指定序列化和反序列化的处理类
+	 * 
+	 * @param factory
+	 * @return
+	 */
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 
-        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+		// 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
-        //使用StringRedisSerializer来序列化和反序列化redis的ke
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+		// 使用StringRedisSerializer来序列化和反序列化redis的ke
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
 
-        //开启事务
-        redisTemplate.setEnableTransactionSupport(true);
+		// 开启事务
+		redisTemplate.setEnableTransactionSupport(true);
 
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
 
-        return redisTemplate;
-    }
-    
+		return redisTemplate;
+	}
+
 }
